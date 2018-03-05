cb3bd30f6e1ce2c5826fb5acb57cbc2774fd22c4
evestorm <1572534835@qq.com>
2018-03-05 12:20:21 +0800

    updated img url

--- assn-restaurant-stage-1/js/dbhelper.js
+++ assn-restaurant-stage-1/js/dbhelper.js
@@ -150,7 +150,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return `/show/assn-restaurant-stage-1/img/${restaurant.photograph}`;
   }
 
   /**
