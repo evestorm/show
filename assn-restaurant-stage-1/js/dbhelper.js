581504c8bc3b9ef54c9d6c0b6ad3b31828a80de1
evestorm <1572534835@qq.com>
2018-03-05 11:52:32 +0800

    updated

--- assn-restaurant-stage-1/js/dbhelper.js
+++ assn-restaurant-stage-1/js/dbhelper.js
@@ -8,8 +8,8 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const port = 8080 // Change this to your server port
+    return `https://evestorm.github.io/show/assn-restaurant-stage-1/data/restaurants.json`;
   }
 
   /**
